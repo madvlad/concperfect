702052be05ba9756cc418bb6dbc92d394920f2a3
David Bohan <dbohan@apterainc.com>
2016-09-09 09:30:26 -0400

    First prototype

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject destination;
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.position = destination.transform.position;
+
+        }
+    }
+}
